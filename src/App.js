2aee7bc614b20bd61de5343028cdd4426fa40075
Thomas Kodair <sunole896@yahoo.com>
2023-03-07 10:42:50 -0800

    app js update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const url = "https://jsonplaceholder.typicode.com/posts/";
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          return Error("Oh no");
+        }
+        return res.json();
+      })
+      .then(data => setPosts(data))
+    console.log("this is log", posts[0]?.title)
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Posts</h1>
+      <h2>...are here</h2>
+
+      {posts.map(post => (
+        <div key={post.id}>{post.title}</div>
+      ))}
     </div>
   );
 }
-
-export default App;
+export default App
