69f9c61715fcdc511d9813c0cdbf60acee69ebb0
sunole <sunole896@yahoo.com>
2023-03-08 11:29:26 -0800

    camera works one time

--- src/App.js
+++ src/App.js
@@ -4,47 +4,55 @@ import { BACKEND_URL } from './enviroment';
 import Html5QrcodePlugin from './Html5QrcodePlugin';
 
 function App() {
-  const [barCodeNumber, setBarCodeNumber] = useState("");
+  // const [barCodeNumber, setBarCodeNumber] = useState("");
   const [items, setItems] = useState([]);
+  const [item, setItem] = useState();
   const url = BACKEND_URL + "items/" 
+  const urlget = BACKEND_URL + "items/" 
   // console.log(url)
-
-  useEffect(() => {
+  const getAllItems = () => {
     fetch(url)
-      .then(res => {
-        if (!res.ok) {
-          return Error("Oh no");
-        }
-        return res.json();
-      })
-      .then(data => setItems(data))
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  // console.log(items)
+    .then(res => {
+      if (!res.ok) {
+        return Error("Oh no");
+      }
+      return res.json();
+    })
+    .then(data => setItems(data))
+  }
+  const getItem = (barCodeNumber) => {
+    fetch(urlget.concat(barCodeNumber))
+    .then(res => {
+      if (!res.ok) {
+        return Error("Oh no");
+      }
+      return res.json();
+    })
+    .then(data => setItem(data))
+  }
+  
   const onNewScanResult = (decodedText, decodedResult) => {
-    // handle decoded results here
-    // console.log(decodedText, decodedResult)
-    setBarCodeNumber(decodedText)
+    getItem(decodedText)
+
+    
   };
-  console.log("empyt", barCodeNumber)
   return (
     <div className="App">
       <Html5QrcodePlugin
-                fps={10}
-                qrbox={250}
-                disableFlip={false}
-                qrCodeSuccessCallback={onNewScanResult}
-            />
-      <h1>Items</h1>
-      <h2>...are here</h2>
-
-      {items.map(item => (
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
+      <h1> Scanned item is: {item?.BarCodeNumber}, {item?.Name}, {item?.Description}, {item?.Response} </h1>
+      <button onClick={() => getAllItems()}> get all items</button>
+      <h1> All Items are:</h1>
+      {items?.map((item) => (
         <div key={item._id}>
-        <div>{item.BarCodeNumber}</div>
-        <div >{item.Name}</div>
-        <div >{item.Description}</div>
-        <div >{item.Response}</div>
+          <div>{item.BarCodeNumber}</div>
+          <div>{item.Name}</div>
+          <div>{item.Description}</div>
+          <div>{item.Response}</div>
         </div>
       ))}
     </div>
